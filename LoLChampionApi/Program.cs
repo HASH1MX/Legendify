e85110d6e3c85b6301c21466c0a988e0570060e4
HASH1MX <hashim8work@gmail.com>
2026-02-20 17:04:41 +0100

    added swagger ui.

--- LoLChampionApi/Program.cs
+++ LoLChampionApi/Program.cs
@@ -5,11 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IChampionRepository, InMemoryChampionRepository>();
 builder.Services.AddScoped<IChampionService, ChampionService>();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
